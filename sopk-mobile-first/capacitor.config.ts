881d31597de87e3a2ab8de57a1fdb1f00569750b
williamkentaandre <williamkentaandre@gmail.com>
2026-05-05 16:04:10 +0200

    go

---
+++ sopk-mobile-first/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nutrisopk.app',
+  appName: 'NutriSOPK',
+  webDir: 'out'
+};
+
+export default config;
